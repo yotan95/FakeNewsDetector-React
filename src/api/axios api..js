b2483568e447b798c5495b30a5937e5a6b91ddac
yotan9508 <yotan9508@gmail.com>
2025-02-02 13:37:12 +0900

    gitlab, dockerfile added

--- src/api/axios api..js
+++ src/api/axios api..js
@@ -1,8 +1,13 @@
 import axios from 'axios';
 
 
+const API_BASE_URL =
+  window.location.hostname === "localhost"
+    ? "http://localhost:8080" // 로컬 환경
+    : "http://3.34.253.76:8080"; // 배포된 Spring 서버
+
 export const httpApi = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: API_BASE_URL,
     headers: {
         'Content-type': 'application/json',
     },
