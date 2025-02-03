f7a516be45ba169ba0e630ac4cdc08280314a2d9
jongjin <kakao1302@naver.com>
2025-02-03 13:08:12 +0900

    aixos ip 변경경

--- src/api/axios api..js
+++ src/api/axios api..js
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 
 const API_BASE_URL =
-  window.location.hostname === "localhost"
-    ? "http://localhost:8080" // 로컬 환경
-    : "http://3.34.253.76:8080"; // 배포된 Spring 서버
+    window.location.hostname === "localhost"
+        ? "http://localhost:8080" // 로컬 환경
+        : "http://3.36.77.31:8080"; // 배포된 Spring 서버
 
 export const httpApi = axios.create({
     baseURL: API_BASE_URL,
