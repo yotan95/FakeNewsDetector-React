fdd82331ba4c3f263d89ad3760998fee45ed5219
yotan9508 <yotan9508@gmail.com>
2025-02-11 13:14:04 +0900

    add service page

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -17,6 +17,9 @@ export function Header() {
                     <img className="icon" alt="" src="/logo.png" />
                 </Link>
                 <div className="group">
+                    <Link to="/introduce" style={{ textDecoration: 'none' }}>
+                        <b className='b'> 서비스 소개</b>
+                    </Link>
                     <Link to="/pricing" style={{ textDecoration: 'none' }}>
                         <b className="b">요금안내</b>
                     </Link>
